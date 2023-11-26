ddb4e34942e6e3859e0faa627466427b3e37ad4a
Hilpy <hilpy00@mail.ru>
2023-11-26 15:31:58 +0300

    Добавьте файлы проекта.

---
+++ dz1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение X: ");
+            double x = double.Parse(Console.ReadLine());
+            double S = 0;
+            double A = x;
+            int n = 0;
+            const double E = 0.001;
+            do
+            {
+                S += A;
+                n++;
+                A = A * 1/n;
+                Console.WriteLine(A);
+            }
+            while (A >= E);
+            Console.WriteLine($"Сумма: {S:f6} Число итераций: {n}");
+        }
+    }
+}
